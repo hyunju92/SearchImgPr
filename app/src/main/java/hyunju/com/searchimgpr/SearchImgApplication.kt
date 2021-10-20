d5d6baa23aac2aa71f0ce615a98bd18979f3af7b
leehyunju <hyunju_e521@hanmail.net>
2021-10-21 00:17:24 +0900

    [프로젝트 환경 설정] Hilt dependecy

--- app/src/main/java/hyunju/com/searchimgpr/SearchImgApplication.kt
+++ app/src/main/java/hyunju/com/searchimgpr/SearchImgApplication.kt
@@ -1,6 +1,8 @@
 package hyunju.com.searchimgpr
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SearchImgApplication : Application() {
 }
--- app/src/main/java/hyunju/com/searchimgpr/main/MainActivity.kt
+++ app/src/main/java/hyunju/com/searchimgpr/main/MainActivity.kt
@@ -3,9 +3,11 @@ package hyunju.com.searchimgpr.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import dagger.hilt.android.AndroidEntryPoint
 import hyunju.com.searchimgpr.R
 import hyunju.com.searchimgpr.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
