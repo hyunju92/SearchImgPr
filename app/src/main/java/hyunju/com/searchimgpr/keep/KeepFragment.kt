4ac6bb02924c9976cbb37aeca70da5a88e024955
leehyunju <hyunju_e521@hanmail.net>
2021-10-21 23:09:47 +0900

    [view 생성] 검색, 보관 frag 생성

--- app/src/main/java/hyunju/com/searchimgpr/keep/KeepFragment.kt
+++ app/src/main/java/hyunju/com/searchimgpr/keep/KeepFragment.kt
@@ -1,6 +1,22 @@
 package hyunju.com.searchimgpr.keep
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import hyunju.com.searchimgpr.R
+import hyunju.com.searchimgpr.databinding.FragmentKeepBinding
 
-class KeepFragment : Fragment(){
+class KeepFragment : Fragment() {
+
+    private lateinit var binding: FragmentKeepBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate<FragmentKeepBinding>(inflater, R.layout.fragment_keep, container, false).apply {
+
+        }
+        return binding.root
+    }
 }
--- app/src/main/java/hyunju/com/searchimgpr/search/SearchFragment.kt
+++ app/src/main/java/hyunju/com/searchimgpr/search/SearchFragment.kt
@@ -1,6 +1,23 @@
 package hyunju.com.searchimgpr.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import hyunju.com.searchimgpr.R
+import hyunju.com.searchimgpr.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment(){
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate<FragmentSearchBinding>(inflater, R.layout.fragment_search, container, false).apply {
+
+        }
+        return binding.root
+    }
+
 }
