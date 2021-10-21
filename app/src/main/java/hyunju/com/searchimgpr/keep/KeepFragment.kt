480ef79fa5913e186b976a98d687c6b7ebccc648
leehyunju <hyunju_e521@hanmail.net>
2021-10-21 22:26:31 +0900

    [view 생성] Main 뷰페이저/탭 생성

---
+++ app/src/main/java/hyunju/com/searchimgpr/keep/KeepFragment.kt
@@ -0,0 +1,6 @@
+package hyunju.com.searchimgpr.keep
+
+import androidx.fragment.app.Fragment
+
+class KeepFragment : Fragment(){
+}
--- app/src/main/java/hyunju/com/searchimgpr/main/MainActivity.kt
+++ app/src/main/java/hyunju/com/searchimgpr/main/MainActivity.kt
@@ -3,9 +3,15 @@ package hyunju.com.searchimgpr.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import hyunju.com.searchimgpr.R
 import hyunju.com.searchimgpr.databinding.ActivityMainBinding
+import hyunju.com.searchimgpr.keep.KeepFragment
+import hyunju.com.searchimgpr.search.SearchFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,5 +22,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        initView()
+    }
+
+    private fun initView() {
+        val tabs = listOf(SearchFragment(), KeepFragment())
+        val tabNames = listOf("검색", "보관")
+
+        binding.mainViewPager.apply {
+            adapter = MainViewPagerAdapter(tabs, this@MainActivity)
+        }
+
+        TabLayoutMediator(binding.mainTab, binding.mainViewPager, true, false) { tab, position ->
+            tab.text = tabNames[position]
+        }.attach()
+    }
+
+    inner class MainViewPagerAdapter(private val tabs: List<Fragment>, fragment: FragmentActivity) :
+        FragmentStateAdapter(fragment) {
+        override fun createFragment(position: Int): Fragment = tabs[position]
+        override fun getItemCount(): Int = tabs.size
     }
 }
