18ab69c86692442840780411f21c95a03b15a066
leehyunju <hyunju_e521@hanmail.net>
2021-10-24 03:59:36 +0900

    [repository] - 이미지, 동영상 검색 api 동시 요청 및 response list merge - list datetime으로 내림차순 sort

--- app/src/main/java/hyunju/com/searchimgpr/search/model/SearchPagingSource.kt
+++ app/src/main/java/hyunju/com/searchimgpr/search/model/SearchPagingSource.kt
@@ -6,31 +6,41 @@ import androidx.paging.PagingState
 import hyunju.com.searchimgpr.search.model.SearchRepository.Companion.DEFAULT_PAGE_INDEX
 import hyunju.com.searchimgpr.search.model.SearchRepository.Companion.DEFAULT_PAGE_SIZE
 import hyunju.com.searchimgpr.search.network.SearchNetworkApi
-import hyunju.com.searchimgpr.search.network.toListSearchUi
+import hyunju.com.searchimgpr.search.network.compareDateStringForDec
+import hyunju.com.searchimgpr.search.network.imgToListSearchUi
+import hyunju.com.searchimgpr.search.network.vclipToListSearchUi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import retrofit2.HttpException
 import java.io.IOException
 import java.lang.Exception
 
 @ExperimentalCoroutinesApi
-class SearchPagingSource(private val searchText: String, private val searchNetworkApi: SearchNetworkApi) :
-    PagingSource<Int, SearchData>() {
+class SearchPagingSource(
+    private val searchText: String,
+    private val searchNetworkApi: SearchNetworkApi
+) : PagingSource<Int, SearchData>() {
 
     @SuppressLint("CheckResult")
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchData> {
         val page = params.key ?: DEFAULT_PAGE_INDEX
 
         return try {
-            val reqParam: Map<String, String> = hashMapOf("page" to page.toString(),"query" to searchText, "size" to DEFAULT_PAGE_SIZE.toString())
-            val response = searchNetworkApi.getImageData(reqParam)
-            val body = response.body()!!.documents.toListSearchUi()
+            val reqParam = getReqParam(searchText, page)
+
+            val imgRes = searchNetworkApi.getImageData(reqParam).body()!!
+                .documents.imgToListSearchUi()
+            val vclipRes = searchNetworkApi.getVclipData(reqParam).body()!!
+                .documents.vclipToListSearchUi()
+
+            val joinedList = (imgRes + vclipRes).sortedWith { a, b ->
+                a.dateTime.compareDateStringForDec(b.dateTime)
+            }
 
             LoadResult.Page(
-                body,
+                joinedList,
                 if (page == DEFAULT_PAGE_INDEX) null else page - 1,
-                if (body.isEmpty()) null else page + 1
+                if (joinedList.isEmpty()) null else page + 1
             )
-
 
         } catch (exception: IOException) {
             return LoadResult.Error(exception)
@@ -39,6 +49,15 @@ class SearchPagingSource(private val searchText: String, private val searchNetwo
         } catch (exception: Exception) {
             return LoadResult.Error(exception)
         }
+    }
+
+    private fun getReqParam(searchText: String, page: Int) : Map<String,String>{
+        return hashMapOf(
+            "query" to searchText,
+//            "sort" to "recency",
+            "page" to page.toString(),
+            "size" to (DEFAULT_PAGE_SIZE / 2).toString()
+        )
     }
 
     override fun getRefreshKey(state: PagingState<Int, SearchData>): Int? {
--- app/src/main/java/hyunju/com/searchimgpr/search/network/SearchReqResData.kt
+++ app/src/main/java/hyunju/com/searchimgpr/search/network/SearchReqResData.kt
@@ -1,10 +1,12 @@
 package hyunju.com.searchimgpr.search.network
 
+import android.os.Build
 import hyunju.com.searchimgpr.search.model.SearchData
+import java.text.SimpleDateFormat
+import java.util.*
 
+// ResData
 // Image Data
-data class ReqImageData(val query: String, val sort: String?, val page: Int?, val size: Int?)
-
 data class ResImageData(val meta: Meta, val documents: List<ImageDocuments>)
 
 data class Meta(val total_count: Int, val pageable_count: Int, val is_end: Boolean)
@@ -20,10 +22,7 @@ data class ImageDocuments(
     val datetime: String
 )
 
-
 // Vclip Data
-data class ReqVclipData(val query: String, val sort: String?, val page: Int?, val size: Int?)
-
 data class ResVclipData(val meta: Meta, val documents: List<VclipDocuments>)
 
 data class VclipDocuments(
@@ -35,31 +34,50 @@ data class VclipDocuments(
     val author: String
 )
 
-
 // mapper
-fun List<ImageDocuments>.toListSearchUi(): List<SearchData> {
+fun List<ImageDocuments>.imgToListSearchUi(): List<SearchData> {
     return map {
-        it.toSearchUiData()
+        SearchData(
+            it.thumbnail_url,
+            it.image_url,
+            it.datetime,
+            "IMG"
+        )
     }
 }
 
-fun ImageDocuments.toSearchUiData() : SearchData {
-    return SearchData(
-        thumbnail_url,
-        image_url,
-        datetime,
-        "IMG"
-    )
+fun List<VclipDocuments>.vclipToListSearchUi(): List<SearchData> {
+    return map {
+        SearchData(
+            it.thumbnail,
+            it.url,
+            it.datetime,
+            "IMG"
+        )
+    }
 }
 
-fun VclipDocuments.toSearchUiData() : SearchData {
-    return SearchData(
-        thumbnail,
-        thumbnail,
-        datetime,
-        "VCLIP"
-    )
+fun String.compareDateStringForDec(otherDateString: String) : Int{
+    val date1 = this.dateStringtoDate()
+    val date2 = otherDateString.dateStringtoDate()
+
+    return if(date1.before(date2)) {
+        1
+    } else {
+        -1
+    }
 }
+
+fun String.dateStringtoDate(): Date {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").parse(this)
+    } else {
+        val newString = this.substring(0, this.length - 6)
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse(newString)
+    }
+}
+
+
 
 
 
